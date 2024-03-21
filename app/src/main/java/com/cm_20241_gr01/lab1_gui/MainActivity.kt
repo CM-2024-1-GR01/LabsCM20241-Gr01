43f8a892c3eabc34b51c97df4ffede57811eaa69
JoseMagg <jose.gonzalezg1@udea.edu.co>
2024-03-21 09:28:41 -0500

    initial commit

---
+++ app/src/main/java/com/cm_20241_gr01/lab1_gui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.cm_20241_gr01.lab1_gui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cm_20241_gr01.lab1_gui.ui.theme.Lab1GUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab1GUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab1GUITheme {
+        Greeting("Android")
+    }
+}
